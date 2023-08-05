ee084256ebf1f969c3a4745b4aa2e4a290652a25
believer4U- <saady6nov@gmail.com>
2023-08-05 09:24:51 +0100

    set up a new branch to set up api

---
+++ apiSetup/src/test/java/com/example/apiSetup/ApiSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
