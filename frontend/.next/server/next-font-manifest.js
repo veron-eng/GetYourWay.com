071c8695850ddca89c9e1b74d1533d53f8867f34
abTaibi98 <139547376+abTaibi98@users.noreply.github.com>
2023-08-13 12:22:56 +0100

    Pushed feature to dev

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/abdul/Documents/GitHub/GetYourWay.com/frontend/src/app/layout\":[\"static/media/939b5075d50998ef-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
