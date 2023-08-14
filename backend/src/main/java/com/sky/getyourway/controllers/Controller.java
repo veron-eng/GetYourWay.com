b1f59aa265d190fee7c3a932d89aa8da918c93c7
veron-eng <veronsundaram13@gmail.com>
2023-08-14 13:02:20 +0100

    added response entities

--- backend/src/main/java/com/sky/getyourway/controllers/Controller.java
+++ backend/src/main/java/com/sky/getyourway/controllers/Controller.java
@@ -5,6 +5,8 @@ import com.sky.getyourway.DTOs.Location;
 import com.sky.getyourway.services.QueryResultsService;
 import com.sky.getyourway.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +19,37 @@ public class Controller {
     @Autowired
     LocationService locationService;
 
+//    @GetMapping("/getFlights/{source}/{destination}/{departure}/{ret}")
+//    public QueryResult getFlights(
+//            @PathVariable("source") String source,
+//            @PathVariable("destination") String destination,
+//            @PathVariable("departure") String departure,
+//            @PathVariable("ret") String ret
+//    ){
+//        return queryResultsService.getJourney(source, destination, departure, ret);//dataToFrondEnd
+//    }
+
     @GetMapping("/getFlights/{source}/{destination}/{departure}/{ret}")
-    public QueryResult getFlights(
+    public ResponseEntity<QueryResult> getFlights(
             @PathVariable("source") String source,
             @PathVariable("destination") String destination,
             @PathVariable("departure") String departure,
             @PathVariable("ret") String ret
     ){
-        return queryResultsService.getJourney(source, destination, departure, ret);//dataToFrondEnd
+        QueryResult queryResult = queryResultsService.getJourney(source, destination, departure, ret);//dataToFrondEnd
+        return ResponseEntity.ok(queryResult);
     }
 
+//    @GetMapping("/getLocation/{code}")
+//    public Location getLocation(@PathVariable("code") String code){
+//        System.out.println(locationService.getLocation(code));
+//        return locationService.getLocation(code);
+//    }
+
     @GetMapping("/getLocation/{code}")
-    public Location getLocation(@PathVariable("code") String code){
+    public ResponseEntity<Location> getLocation(@PathVariable("code") String code){
         System.out.println(locationService.getLocation(code));
-        return locationService.getLocation(code);
+        Location location = locationService.getLocation(code);
+        return ResponseEntity.ok(location);
     }
 }
