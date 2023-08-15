d63c0caa41565c63b3172fee21d6576896b3dae4
t77bsh <taabish_khan22@hotmail.co.uk>
2023-08-15 12:56:04 +0100

    added more tests

--- frontend/__tests__/searchPage.test.js
+++ frontend/__tests__/searchPage.test.js
@@ -1,21 +1,21 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import Search from "../src/app/page";
-import { useRouter } from 'next/navigation'
-import { act } from 'react-dom/test-utils';
-import { create } from 'react-test-renderer'
+import { useRouter } from "next/navigation";
+import { act } from "react-dom/test-utils";
+import { create } from "react-test-renderer";
 
 // Mocks
-jest.mock('next/navigation', () => ({
-  useRouter: jest.fn()
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
 }));
 
 const pushMock = jest.fn();
 useRouter.mockReturnValue({ push: pushMock });
 
 global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve({ key: 'value' }),
-    })
+  Promise.resolve({
+    json: () => Promise.resolve({ key: "value" }),
+  })
 );
 
 describe("SearchComponent", () => {
@@ -25,28 +25,40 @@ describe("SearchComponent", () => {
     const button = getByText("Search Flights");
     fireEvent.click(button);
 
-    expect(pushMock).toHaveBeenCalledWith('/results?from=&to=&leave=&ret=')
+    expect(pushMock).toHaveBeenCalledWith("/results?from=&to=&leave=&ret=");
   });
 
   it("should take in correct values from input fields for the results path", async () => {
-    const testRenderer = create(<Search />)
-    const testInstance = testRenderer.root
+    const testRenderer = create(<Search />);
+    const testInstance = testRenderer.root;
 
-    const fromAirport = testInstance.findByProps({id: 'fromAirport'})
-    await act(async () => fromAirport.props.onChange({target: {value: 'London Heathrow Airport (LHR)'}}))
+    const fromAirport = testInstance.findByProps({ id: "fromAirport" });
+    await act(async () =>
+      fromAirport.props.onChange({
+        target: { value: "London Heathrow Airport (LHR)" },
+      })
+    );
 
-    const toAiport = testInstance.findByProps({id: 'toAirport'})
-    await act(async () => toAiport.props.onChange({target: {value: 'London Gatwick Airport (LGW)'}}))
+    const toAiport = testInstance.findByProps({ id: "toAirport" });
+    await act(async () =>
+      toAiport.props.onChange({
+        target: { value: "London Gatwick Airport (LGW)" },
+      })
+    );
 
-    const departDate = testInstance.findByProps({id: 'departDate'})
-    await act(async () => departDate.props.onChange(new Date(2023, 9, 15)))
+    const departDate = testInstance.findByProps({ id: "departDate" });
+    await act(async () => departDate.props.onChange(new Date(2023, 9, 15)));
 
-    const returnDate = testInstance.findByProps({id: 'returnDate'})
-    await act(async () => returnDate.props.onChange(new Date(2023, 9, 20)))
+    const returnDate = testInstance.findByProps({ id: "returnDate" });
+    await act(async () => returnDate.props.onChange(new Date(2023, 9, 20)));
 
-    const button = testInstance.findByProps({ id: 'searchButton' })
-    await act(async () => button.props.onClick(new Event('search button pressed')))
+    const button = testInstance.findByProps({ id: "searchButton" });
+    await act(async () =>
+      button.props.onClick(new Event("search button pressed"))
+    );
 
-    expect(pushMock).toHaveBeenCalledWith('/results?from=LHR&to=LGW&leave=2023-10-15&ret=2023-10-20')
+    expect(pushMock).toHaveBeenCalledWith(
+      "/results?from=LHR&to=LGW&leave=2023-10-15&ret=2023-10-20"
+    );
   });
 });
