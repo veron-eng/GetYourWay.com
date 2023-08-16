b0a4fd91e32b5062592480e634461a3097f986cf
PaoloMura <paolo@muraonline.com>
2023-08-16 16:18:37 +0100

    adapted the test for the new dropdown changes

--- frontend/__tests__/searchPage.test.js
+++ frontend/__tests__/searchPage.test.js
@@ -21,18 +21,19 @@ global.fetch = jest.fn(() =>
 describe("SearchComponent", () => {
 
   it("should redirect correctly when 'Search Flights' button is clicked", () => {
-    const { getByText } = render(<Search />);
+    const { getByText } = render(<Search searchParams={{from: undefined, to: undefined}} />);
 
     const button = getByText("Search Flights");
     fireEvent.click(button);
 
-    expect(pushMock).toHaveBeenCalledWith("/results?from=&to=&leave=&ret=");
+    expect(pushMock).toHaveBeenCalledWith("/results?from=&to=&leave=&ret=&passengers=1");
   });
 
   it("should take in correct values from input fields for the results path", async () => {
-    const testRenderer = create(<Search />);
+    const testRenderer = create(<Search searchParams={{from: undefined, to: undefined}} />);
     const testInstance = testRenderer.root;
 
+    // User changes the from textbox
     const fromAirport = testInstance.findByProps({ id: "fromAirport" });
     await act(async () =>
       fromAirport.props.onChange({
@@ -40,6 +41,7 @@ describe("SearchComponent", () => {
       })
     );
 
+    // User changes the to textbox
     const toAiport = testInstance.findByProps({ id: "toAirport" });
     await act(async () =>
       toAiport.props.onChange({
@@ -47,19 +49,26 @@ describe("SearchComponent", () => {
       })
     );
 
+    // User changes the from date
     const departDate = testInstance.findByProps({ id: "departDate" });
     await act(async () => departDate.props.onChange(new Date(2023, 9, 15)));
 
+    // User changes the to date
     const returnDate = testInstance.findByProps({ id: "returnDate" });
     await act(async () => returnDate.props.onChange(new Date(2023, 9, 20)));
 
+    const dropdown = testInstance.findByProps({ id: "passengers" })
+    await act(async () => dropdown.props.onChange({ target: { value: "2" } }))
+
+    // User presses the search button
     const button = testInstance.findByProps({ id: "searchButton" });
     await act(async () =>
       button.props.onClick(new Event("search button pressed"))
     );
 
+    // Perform assertion
     expect(pushMock).toHaveBeenCalledWith(
-      "/results?from=LHR&to=LGW&leave=2023-10-15&ret=2023-10-20"
+      "/results?from=LHR&to=LGW&leave=2023-10-15&ret=2023-10-20&passengers=2"
     );
 
 
